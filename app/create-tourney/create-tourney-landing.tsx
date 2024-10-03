eec2a2a2a249a9f70abc02f5783c548d23970c12
gkojr <georgekowusujr@gmail.com>
2024-10-03 11:00:31 -0400

    created tourney landing page for create

--- app/create-tourney/create-tourney-landing.tsx
+++ app/create-tourney/create-tourney-landing.tsx
@@ -7,8 +7,17 @@ const CreateTourneyLanding = () => {
 
 
     return (
-        <div>
-            
+        <div id="create-tourney-landing-page">
+            <div id="create-tourney-images">
+
+            </div>
+            <div id="create=tourney-text">
+
+            </div>
+            <div id="create-tourney-footer">
+                
+            </div>
+
         </div>
 
 
